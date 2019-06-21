8687e7d93e634321eb5a7ee4fa6fb20ad1ff15cb
akashsingh41 <akashsingh41@hotmail.com>
2019-06-21 22:09:53 +0530

    jQuery Ajax In ASP.NET MVC

---
+++ jQueryAjaxInASP.NETMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jQueryAjaxInASP.NETMVC.Startup))]
+namespace jQueryAjaxInASP.NETMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
